44c7e563e9153f9df8289e4a0c6287f03d1a100c
dirmeier <simon.dirmeier@bsse.ethz.ch>
2018-07-07 17:44:52 +0200

    added eta computation and some fixes

--- R/glmer.R
+++ R/glmer.R
@@ -1,11 +1,8 @@
-
-
-
 #' @title Initialize the linear predictor for generalized linear effect models
 #'
 #' @description \code{greta.glmer} creates the linear predictor $eta$ required
 #' for fitting generalized linear mixed models. The linear predictor has the
-#' form: \deqn{\eta = X \beta + Z \gamma},
+#' form: \deqn{\eta = X \beta + Z \gamma,}
 #' where \eqn{X} is the fixed effects design matrix, \eqn{\beta} the fixed
 #' effects, \eqn{Z} the random effects design matrix and \eqn{\gamma} the
 #' random effects.
@@ -22,14 +19,14 @@
 #'  that specifies the prior for the intercept \eqn{\beta_0} for the
 #'  fixed effects design
 #'  matrix. If \code{NULL}, the distribution for the intercept is chosen as
-#'  \deqn{p(\beta_0) \propto const}. The intercept prior
+#'  \deqn{p(\beta_0) \propto const.} The intercept prior
 #'  needs to have dimension 1.
 #' @param  prior_coefficients  a \code{\link{greta::greta_array}} random
 #'  variable that specifies the prior for the coefficients\eqn{\beta} for the
 #'  fixed effects
 #'  design matrix. If \code{NULL}, the distribution for the coefficients is
-#'  chosen as \deqn{p(\beta_i) \sim \mathcal{N}(0, \sigma_{\beta})}, and
-#'   \deqn{p(\sigma_{\beta}) \sim \text{Half-Cauchy}(0, \inf)}.
+#'  chosen as \deqn{p(\beta_i) \sim \mathcal{N}(0, \sigma_{\beta}),} and
+#'   \deqn{p(\sigma_{\beta}) \sim \text{Half-Cauchy}(0, \inf).}
 #'  The coefficients prior
 #'  need to have the same dimensionalty as the number of columns of
 #'  your fixed effects design matrix (without the intercept).
@@ -37,13 +34,13 @@
 #'  variable that specifies the prior for the random effects \eqn{\beta} for
 #'  the random effects design matrix. If \code{NULL}, the distribution for the
 #'   random effects is chosen as
-#'   \deqn{p(\gamma_g) \sim \mathcal{N}(0, \sigma_{\gamma_g})}, and
-#'   \deqn{p(\sigma_{\gamma_g}) \sim \text{Wishart}(\text{len}(\gamma_g) + 1, \text{diag}(\text{len}(\gamma_g)))}.
+#'   \deqn{p(\gamma_g) \sim \mathcal{N}(0, \sigma_{\gamma_g}),} and
+#'   \deqn{p(\sigma_{\gamma_g}) \sim \text{Wishart}(\text{len}(\gamma_g) + 1, \text{diag}(\text{len}(\gamma_g))).}
 #'  If \code{prior_random_effects} is provided
 #'  it needs to have the same dimensionalty as the
 #'  number of columns of \eqn{Z}
 #'  random effect design matrix. These are generally not trivial to construct.
-#'  Calling \code{lme4::glFormula(formula, data)} creates the specified
+#'  Calling \code{\link{lme4::glFormula(formula, data)} creates the specified
 #'  random effects matrix \eqn{Z}. From this the dimensionality of
 #'  \code{prior_random_effects} can be inferred.
 #'
@@ -81,35 +78,20 @@ greta.glmer <- function(
 
 .model <- function(mod, prior_intercept, prior_coefficients, prior_random_effects)
 {
-  X <- greta::as_data(as.matrix(mod$X))
-  p <- ncol(X)
+  coef_list  <- .coef_priors(mod, prior_intercept, prior_coefficients)
+  ranef_list <- .ranef_priors(mod, prior_random_effects)
 
-  coef_list  <- .coef_priors(p, prior_intercept, prior_coefficients)
-  ranef_list <- .ranef_priors(mod, p, prior_random_effects)
-  eta <- X %*% ret$coef + ranef_list$ranef_eta
-
-  ret <- c(coef_list, ranef_list)
-  ret$formula <- mod$mormula
-  ret$predictor <- eta
-
-  s <- c(
-    predictor   = eta,
-    ranef = gamma,
-    ranef_sd   = gamma_sd,
-    formula    = mod$formula,
-    groups  = groups,
-    Z   = Z,
-    Ztlist = ztlist
-  )
+  ret           <- c(coef_list, ranef_list)
+  ret$formula   <- mod$mormula
+  ret$predictor <- coef_list$eta + ranef_list$eta
 
   ret
 }
 
 
-.ranef_priors <- function(mod, p, prior_random_effects)
+.ranef_priors <- function(mod, prior_random_effects)
 {
-
-  Z       <- as.matrix(t(mod$reTrms$Zt))
+  Z       <- t(as.matrix(mod$reTrms$Zt))
   Ztlist  <- mod$reTrms$Ztlist
   Zp      <- ncol(Z)
   grp.vars   <- mod$reTrms$flist
@@ -120,49 +102,64 @@ greta.glmer <- function(
     desc <- prior_random_effects$node$description()
     if (!length(grep("normal", desc)))
       stop("Random effects prior does not follow a normal distribution", call.=FALSE)
-    check.dimensionality(dim(prior_random_effects), p)
+    check.dimensionality(dim(prior_random_effects), c(Zp, 1))
   }
 
-
-  gamma    <- greta::zeros(Zp)
   gamma_sd <- greta::zeros(Zp, Zp)
+  gamma <- if (is.null(prior_random_effects)) {
+    greta::zeros(Zp)
+  } else {
+    prior_random_effects
+  }
 
   eta <- 0
-  idxs <- 1
-  for (i in seq(length(Ztlist)))
+  idxs  <- 1
+  for (i in seq_len(length(Ztlist)))
   {
-    zt          <- ztlist[[i]]
+    # current random term
+    zt          <- Ztlist[[i]]
+    # grouping levels for that term
     zt.levels   <- rownames(zt)
+    # unique grouping levels, i.e. how which groups for this term are there
+    # TODO: can this be done nicer using `lme4::glFormula` elements?
     zt.u.levels <- unique(zt.levels)
 
+    # iterate over the different groups
     for (zt.u.level in zt.u.levels)
     {
-      zt.level_p <- sum(zt.u.level == zt.levels)
+      # dimensionality of the random effects for the current group
+      zt.level_p   <- sum(zt.u.level == zt.levels)
       zt.level.idx <- which(zt.u.level == zt.levels)
-      ranef_sd   <- greta::wishart(zt.level_p + 1, diag(zt.level_p))
 
-      if (p == 1)
-      {
-        ranef_coef <- greta::multivariate_normal(rep(0, zt.level_p), ranef_sd)
-      }
-      else
-      {
-        ranef_coef <- greta::normal(0, ranef_sd)
+      # we just use a wishart, because it creates an psd matrix
+      ranef_sd   <- solve(greta::wishart(zt.level_p + 1, diag(zt.level_p)))
+      ranef_coef <- if (zt.level_p > 1) {
+        greta::multivariate_normal(rep(0, zt.level_p), ranef_sd)
+      } else {
+        greta::normal(0, ranef_sd)
       }
 
-      gamma[seq(idxs, idxs + zt.level_p - 1)] <- ranef_coef
-      gamma_sd[seq(idxs, idxs + zt.level_p - 1), seq(idxs, idxs + zt.level_p - 1)] <- ranef_sd
-      eta <- eta + as.matrix(t(zt))[,zt.level.idx, drop=FALSE] %*% ranef_coef
+      # if random effects prior is not provided, create a random variable and
+      # an rv for the covariance
+      if (is.null(prior_random_effects)) {
+        gamma_sd[seq(idxs, idxs + zt.level_p - 1), seq(idxs, idxs + zt.level_p - 1)] <- ranef_sd
+        gamma[seq(idxs, idxs + zt.level_p - 1)] <- ranef_coef
+      }
+
+      # add design matrix * random effect to the linear predictor
+      z <- t(as.matrix(zt))[,zt.level.idx, drop=FALSE]
+      eta <- eta + z %*% gamma[seq(idxs, idxs + zt.level_p - 1)]
       idxs <- idxs + zt.level_p
-
     }
   }
 
   ret <- list(
     eta = eta,
     gamma = gamma,
-    gamma_sd
-  )
+    Z = Z,
+    Ztlist = Ztlist,
+    grp.vars = grp.vars)
+  if (is.null(prior_random_effects)) ret$gamma_sd <- gamma_sd
 
   ret
 }
@@ -173,10 +170,13 @@ greta.glmer <- function(
 #' @noRd
 #' @importFrom methods is
 #' @importFrom greta normal variable zeros
-.coef_priors <- function(p,  prior_intercept, prior_coefficients)
+.coef_priors <- function(mod, prior_intercept, prior_coefficients)
 {
 
-  ret <- list()
+  X <- greta::as_data(as.matrix(mod$X))
+  p <- ncol(X)
+
+  ret <- list(X=X)
   coef_prior <- greta::zeros(p)
 
   if (is.null(prior_intercept))
@@ -205,5 +205,7 @@ greta.glmer <- function(
   }
 
   ret$coef <- coef
+  ret$eta <- X %*% coef
+
   ret
 }
